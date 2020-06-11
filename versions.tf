03f9a4c2c7be838da018d1c2877ae9b86f1f813d
Mark <mark.binlab@gmail.com>
2020-06-11 20:19:26 +0300

    Fix version dependency control behavior

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 0.12"
   required_providers {
-    aws      = "~> 2.53.0"
-    tls      = "~> 2.1.1"
-    ignition = "~> 1.2.1"
+    aws      = ">= 2.53.0"
+    tls      = ">= 2.1.1"
+    ignition = ">= 1.2.1"
   }
 }
